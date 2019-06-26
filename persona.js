1b8e34dd05513287963d384d76d91bf07b21a4a1
matorres0715 <matorres0715@misena.edu.co>
2019-06-26 11:57:31 -0500

    commit

---
+++ persona.js
@@ -0,0 +1,87 @@
+//var cadena1="carro bmw 2019"
+// var cadena2="nuevo"
+// console.log(cadena1.length)
+// console.log(cadena1.charAt(0))
+// console.log(cadena1.charAt(1))
+// console.log(cadena1.charAt(2))
+// console.log(cadena1.charAt(3))
+// console.log(cadena1.charAt(4))
+
+//este sirve para mostrar en minuscula
+// console.log(cadena1.toLowerCase())
+//este sirve para mostrar en minuscula
+// console.log(cadena1.toUpperCase())
+
+// var cadenanueva=cadena1.concat(cadena2)
+// console.log(cadenanueva)
+
+//este sirve para mostrar si es falso o verdadero
+// console.log(cadena1.endsWith("a"))
+//este sirve para mostrar si es falso o verdadero
+// console.log(cadena1.includes("rp"))
+
+//este muestra en que posicion se encuentra la palabra
+// console.log(cadena1.indexOf("bmw"))
+//esto es para mostrar o hasta la 5 la palabra
+//console.log(cadena1.slice(0,5))
+
+var cadena1="carro bmw"
+
+console.log(cadena1.substring(4,6))
+//esto es para convertir un arreglo en un array con un separador
+//console.log(cadena1.split(" "))
+
+
+
+
+
+// var arreglopersonas=[
+
+// {
+// 	nombre:"camila",
+// 	apellido:"torres",
+// 	edad:15,
+// 	cc:123456,
+// 	peliculaFavorita:"era de hielo",
+// 	genero:"animada"
+// },
+// {
+// 	nombre:"celeste",
+// 	apellido:"sanchez",
+// 	edad:25,
+// 	cc:12345,
+// 	peliculaFavorita:"titanic",
+// 	genero:"drama"
+// },
+// {
+// 	nombre:"may",
+// 	apellido:"cardona",
+// 	edad:31,
+// 	cc:1234,
+// 	peliculaFavorita:"tres metros bajo el cielo",
+// 	genero:"romantica"
+// }
+// ]
+// for (var i=0; i< arreglopersonas.length;i++)
+//  {
+// 	alert(arreglopersonas[i].nombre+" "+arreglopersonas[i].edad+" "+arreglopersonas[i].genero)
+
+// 	if (arreglopersonas[i].edad<18) 
+// 	{
+// 	alert("te recomiendo que veas peliculas de disney:")
+//     alert("tierra de osos,aladin,dumbo,tarzan")
+// }
+
+// if (arreglopersonas[i].edad >18) 
+// 	{
+// 		alert("te recomiendo que veas peliculas de terror")
+//     alert("annabelle,midsommar,escape")
+// 	}
+	
+//     if (arreglopersona[i].edad>30){
+//     alert("te recomiendo que veas pelicular de comedia romantica")
+//     alert("la propuesta,loco y estupido amor,mi primer amor")
+//     }
+// }
+
+
